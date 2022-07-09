315fa0377ad1dd5dd8bfeb0f98c61dd54e86412a
Yanggi Song <didrl741@gmail.com>
2022-07-09 18:42:50 +0900

    로그인 기능 (웹 계층)
    UserController에 로그인을 담당하는 메서드들 추가, 로그인 실패시 alert 실행기능 추가
    UserService에 로그인 검증기능 추가

--- src/main/java/toyproject/board/controller/UserController.java
+++ src/main/java/toyproject/board/controller/UserController.java
@@ -1,6 +1,7 @@
 package toyproject.board.controller;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,10 +10,14 @@ import org.springframework.web.bind.annotation.PostMapping;
 import toyproject.board.domain.User;
 import toyproject.board.service.UserService;
 
+import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
     private final UserService userService;
@@ -25,6 +30,11 @@ public class UserController {
 
     @PostMapping("/users/new")
     public String create(@Valid UserForm userForm, BindingResult result) {
+
+        if (userForm.getName().length() == 0 || userForm.getPassword().length() == 0) {
+            return "users/createUserForm";
+        }
+
         User user = new User();
 
         user.setUserName(userForm.getName());
@@ -33,5 +43,33 @@ public class UserController {
 
         userService.join(user);
         return "redirect:/";
+    }
+
+    @GetMapping("/users/login")
+    public String loginForm(Model model) {
+        model.addAttribute("userForm", new UserForm());
+        return "users/login";
+    }
+
+    @PostMapping("/users/login")
+    public String login(@Valid UserForm userForm, BindingResult result, HttpSession session, Model model) {
+
+        if (userForm.getName().length() == 0 || userForm.getPassword().length() == 0) {
+            return "users/login";
+        }
+
+        Map<String, Object> rs = userService.checkLoginAvailable(userForm);
+
+        String resultCode = (String) rs.get("resultCode");
+        String msg = (String) rs.get("msg");
+
+        if (resultCode.startsWith("S")) {
+            session.setAttribute("loginedUserName", userForm.getName());
+            return "redirect:/";
+        } else {
+            model.addAttribute("msg", msg);
+            return "users/login";
+        }
+
     }
 }
--- src/main/java/toyproject/board/service/UserService.java
+++ src/main/java/toyproject/board/service/UserService.java
@@ -1,16 +1,21 @@
 package toyproject.board.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import toyproject.board.controller.UserForm;
 import toyproject.board.domain.User;
 import toyproject.board.repository.UserRepository;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
+@Slf4j
 public class UserService {
 
     private final UserRepository userRepository;
@@ -35,5 +40,28 @@ public class UserService {
 
     public User findOne(Long userId) {
         return userRepository.findOne(userId);
+    }
+
+    // 로그인 가능 체크
+    public Map<String, Object> checkLoginAvailable(UserForm userForm) {
+        Map<String, Object> rs = new HashMap<String, Object>();
+
+        List<User> users = userRepository.findByName(userForm.getName());
+
+        if (users.isEmpty()) {
+            rs.put("resultCode", "F-1");
+            rs.put("msg", "해당 회원이 존재하지 않습니다.");
+            log.info("해당 회원이 존재하지 않습니다");
+        } else if (users.get(0).getUserPassword().equals(userForm.getPassword()) == false ) {
+            rs.put("resultCode", "F-2");
+            rs.put("msg", "비밀번호가 일치하지 않습니다.");
+            log.info("비밀번호가 일치하지 않습니다.");
+        } else {
+            rs.put("resultCode", "S-1");
+            rs.put("msg", "로그인에 성공했습니다.");
+            log.info("로그인 성공");
+        }
+
+        return rs;
     }
 }
